376dffaa251aa3b887dee7cee1b0531e77e3c687
Tomas Slavik <tomas.slavik@microsoft.com>
2019-02-04 20:31:38 +0100

    prod/dev insights

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
   apiUrlBackend: 'https://kudos-apim-jmu6m5q6suefk.azure-api.net/v2',
-  apiUrlBackendCors: 'https://kudos-apim-jmu6m5q6suefk.azure-api.net/kudos-func-jmu6m5q6suefk/corsTest?name=chybaTK'
+  apiUrlBackendCors: 'https://kudos-apim-jmu6m5q6suefk.azure-api.net/kudos-func-jmu6m5q6suefk/corsTest?name=chybaTK',
+  appInsightsConfig: {
+    instrumentationKey: '11e0b7ef-2ac3-43a0-ae89-1a44e8e2ae4e',
+  }
 };
